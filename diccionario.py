0e50fe3c6cbf3d5cc86152d2cddffd50c3b08902
ChapetonUD <schapetonk@udistrital.edu.co>
2024-09-07 20:35:10 -0500

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,39 @@
+[
+    {
+      "brazo": {
+        "base": {
+          "nombre": "Base",
+          "dimensiones": {
+            "ancho": 0.50,
+            "alto": 0.22,
+            "profundidad": 0.50
+          }
+        },
+        "hombro": {
+          "nombre": "Hombro",
+          "dimensiones": {
+            "ancho": 0.04,
+            "alto": 0.7,
+            "profundidad": 0.04
+          }
+        },
+        "codo": {
+          "nombre": "Codo",
+          "dimensiones": {
+            "ancho": 0.04,
+            "alto": 0.9,
+            "profundidad": 0.04
+          }
+        },
+        "antebrazo": {
+          "nombre": "Antebrazo",
+          "dimensiones": {
+            "ancho": 0.04,
+            "alto": 0.9,
+            "profundidad": 0.04
+          }
+        }
+        
+      }
+    }
+  ]
