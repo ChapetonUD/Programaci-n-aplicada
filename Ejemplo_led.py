d799d2a7eb84a72439a9b8e37eb643d8fac6c773
ChapetonUD <schapetonk@udistrital.edu.co>
2024-09-28 19:56:19 -0500

    Add files via upload

---
+++ Ejemplo_led.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""Example for Pico. Blinks the built-in LED."""
+import time
+import board
+import digitalio
+
+print(dir(board))
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
+    print('.',end='')
